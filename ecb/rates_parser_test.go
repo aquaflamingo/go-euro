53ac3e915c24ae87cbaa3309a7cf78df29b9db79
robertsimoes <robertsimoes@users.noreply.github.com>
2020-11-07 13:06:22 -0800

    [tests] Adds Historical and 90 tests
    Why:
    
    *
    
    This change addresses the need by:
    
    *
    
    ; 50-character subject line
    ;
    ; 72-character wrapped longer description.

--- ecb/rates_parser_test.go
+++ ecb/rates_parser_test.go
@@ -19,17 +19,19 @@ func setup() func() {
 	return func() {}
 }
 
-type MockECB struct{}
+type MockECB struct {
+	filePath string
+}
 
 func (m *MockECB) GetRatesXml(f Feed) ([]byte, error) {
-	return xmlFixture("2020-11-06.xml"), nil
+	return xmlFixture(m.filePath), nil
 }
 
 func Test_Today(t *testing.T) {
 	teardown := setup()
 	defer teardown()
 
-	mockSource := MockECB{}
+	mockSource := MockECB{filePath: "2020-11-06.xml"}
 
 	ratesParser := NewRatesParser(&mockSource)
 
@@ -50,6 +52,63 @@ func Test_Today(t *testing.T) {
 	usdRate := rates[0]
 	assertMatch(usdRate.IsoCurrency, "USD", t)
 	assertMatch(usdRate.Rate, "1.1870", t)
+}
+
+func Test_NinetyDay(t *testing.T) {
+	teardown := setup()
+	defer teardown()
+
+	mockSource := MockECB{filePath: "hist-90d.xml"}
+
+	ratesParser := NewRatesParser(&mockSource)
+
+	result, err := ratesParser.Today()
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	timeRates := result.BaseCube.TimeCubes
+
+	// Only 65 rates
+	assertIntMatch(len(timeRates), 65, t)
+
+	rates := timeRates[0].RateCubes
+
+	assertIntMatch(len(rates), 32, t)
+
+	usdRate := rates[0]
+	assertMatch(usdRate.IsoCurrency, "USD", t)
+	assertMatch(usdRate.Rate, "1.187", t)
+}
+
+func Test_Historical(t *testing.T) {
+	teardown := setup()
+	defer teardown()
+
+	mockSource := MockECB{filePath: "historical.xml"}
+
+	ratesParser := NewRatesParser(&mockSource)
+
+	result, err := ratesParser.Today()
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	timeRates := result.BaseCube.TimeCubes
+
+	if len(timeRates) <= 90 {
+		t.Fatalf("expected > %d got %d", 90, len(timeRates))
+	}
+
+	rates := timeRates[0].RateCubes
+
+	assertIntMatch(len(rates), 32, t)
+
+	usdRate := rates[0]
+	assertMatch(usdRate.IsoCurrency, "USD", t)
+	assertMatch(usdRate.Rate, "1.187", t)
 }
 
 func assertIntMatch(got int, expect int, t *testing.T) {
